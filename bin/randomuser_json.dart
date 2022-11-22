c6da3213d789ca86f78fc47eb66f9ef1a862c879
ZARIFMAC <uzbzarif@gmail.com>
2022-11-22 14:59:34 +0500

    init

---
+++ bin/randomuser_json.dart
@@ -0,0 +1,5 @@
+import 'package:randomuser_json/randomuser_json.dart' as randomuser_json;
+
+void main(List<String> arguments) {
+  print('Hello world: ${randomuser_json.calculate()}!');
+}
---
+++ test/randomuser_json_test.dart
@@ -0,0 +1,8 @@
+import 'package:randomuser_json/randomuser_json.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
